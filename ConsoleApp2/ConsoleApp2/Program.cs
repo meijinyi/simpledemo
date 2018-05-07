76efe7a6794d13123eb940a451fb249b10bacb89
梅@aliyun <hellomeijy@163.com>
2018-05-07 09:26:15 +0800

    1

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using EFLib;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Z.EntityFramework.Plus;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                var q = db.Locations.Where(t => t.ID == "").Update(t => new Location { Level = LocationLevel.Area });
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/EFLib/Class1.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFLib
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"balbal");
+        }
+    }
+
+    public class Location
+    {
+        [Key]
+        public string ID
+        { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public string ParentID
+        {
+            get;
+            set;
+        }
+
+        public LocationLevel Level
+
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+
+    public enum LocationLevel
+    {
+        Province = 1,
+        City,
+        Area,
+        Section
+    }
+}
